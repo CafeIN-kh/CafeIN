49707e0898f847ed3f8c5b4830ad4392ebce42ac
Park <Park@Park-PC>
2016-09-13 10:07:53 +0900

    [sql insert문 추가] docs/ franchise.sql(프랜차이즈 목록) franchise_menu.sql(프랜차이즈 메뉴 목록)
    2016. 09. 13 오전 10시 07분 저장

---
+++ src/main/webapp/docs/franchise.sql
@@ -0,0 +1,39 @@
+Insert into FRANCHISE (FRANCHISE_NUM,FRANCHISE_NAME,FRANCHISE_IMG,FRANCHISE_INTRODUCE,FRANCHISE_VISIT) values (1,'starbucks','starbucks.png','음료를 전해주기 위해서 카운터 너머로 쭉 뻗은 손 하나가 맞은 편에서 이를 받기 위해 뻗은 다른 손과 만나는 것은 단 한 순간의 일입니다.
+그러나 이는 두 사람의 교감이 이루어지는 순간이기도 합니다.
+“세계 최고 품질의 커피를 제공하겠다는 약속부터 책임 있는 기업이 되기 위해서
+고객 및 지역사회와 교감하는 것까지,
+스타벅스는 모든 일에 있어서 고객과의 관계를 존중할 수 있도록 노력합니다.”
+40여 년 전에 하나의 매장으로 시작한 이래, 우리는 스타벅스가 함께하게 되는 모든 장소 및 지역사회를 처음보다 더 나은 곳으로 만들 수 있도록
+늘 노력하고 있습니다.',304);
+Insert into FRANCHISE (FRANCHISE_NUM,FRANCHISE_NAME,FRANCHISE_IMG,FRANCHISE_INTRODUCE,FRANCHISE_VISIT) values (2,'tomntoms','tomntoms.png','오리지널 스페셜티 커피 전문점 브랜드 TOM N TOMS
+탐앤탐스는 신선하고 풍부한 커피의 맛과 향으로 고객 만족을 위해 최선을 다하고 있습니다. 국내 및 세계 커피전문점 시장을 선도하며
+고품격 커피와 갓 구운 따뜻한 프레즐 그리고 좋은 사람들이 함께하는 편안한 분위기로 지금 대한민국을 비롯해 전 세계에서 사랑 받고 있습니다.',423);
+Insert into FRANCHISE (FRANCHISE_NUM,FRANCHISE_NAME,FRANCHISE_IMG,FRANCHISE_INTRODUCE,FRANCHISE_VISIT) values (3,'ediya','ediya.png','고객을 향한 진심 이디야 커피
+2015년 3월 이디야커피는 커피전문점 최초 국내 1,500호를 오픈하며 전국 매장에서
+고객님들께 품질 좋고 맛있는 커피를 합리적인 가격에 제공해 드리고 있습니다.
+국내 대기업과 해외 브랜드와의 경쟁을 꿋꿋이 이겨낸 이디야커피는 고객님들께 품질좋은 커피와
+진심의 서비스를 드리기 위해 지금 이 순간에도 끊임없이 노력하고 있습니다.',280);
+Insert into FRANCHISE (FRANCHISE_NUM,FRANCHISE_NAME,FRANCHISE_IMG,FRANCHISE_INTRODUCE,FRANCHISE_VISIT) values (4,'hollys','hollys.png','다양한 토양과 기후에서 자란 커피 원두는 그 속에 각기 다른 맛과 향의 비밀을 숨기고 있습니다.
+생두를 로스팅하여 에스프레소를 만드는 과정은 단순하고 평범해 보이지만, 사실은 로스팅과 추출법 등의 미묘하고 작은 차이가 
+커피 맛을 변화시킵니다. 
+ 
+원두에 숨겨진 이러한 커피의 비밀을 하나 하나 밝혀가면서 많은 사람들이 커피와 함께 소중한 순간을 즐기도록 해주는 것이 
+바로 할리스커피가 추구하는 ‘커피의 즐거움’입니다.
+ 
+오늘도 할리스커피는 전세계의 커피 산지를 연구하고, 원두 본연의 맛을 살리기 위한 수많은 블랜딩을 개발하는 등 다양한 
+노력을 지속하고 있습니다. 커피가 주는 아름다움을 보다 많은 고객들이 즐길 수 있도록, 매 순간 의미 있는 작은 차이를 
+만들어가는 할리스커피가 되겠습니다.',48);
+Insert into FRANCHISE (FRANCHISE_NUM,FRANCHISE_NAME,FRANCHISE_IMG,FRANCHISE_INTRODUCE,FRANCHISE_VISIT) values (5,'gongcha','gongcha.png','공차는 원산지의 특별한 향이 느껴지는 양질의 차 잎을 정성스럽게 우려내어 만듭니다. 공차는 고객들의 건강을 위해 음료의 원재료부터 가공방법까지 신중하게 고려하고 있습니다. 공차는 다양한 메뉴와 즐거운 선택을 통해 남녀노소 누구나 쉽게 즐기는 차 문화를 만들어 가겠습니다.
+',13);
+Insert into FRANCHISE (FRANCHISE_NUM,FRANCHISE_NAME,FRANCHISE_IMG,FRANCHISE_INTRODUCE,FRANCHISE_VISIT) values (6,'coffeebean','coffeebean.png','(주)커피빈코리아는 미국 International Coffee 티, LLC 와 프랜차이즈 계약을 맺어 
+2000년 6월 설립된 회사로서 최상급의 커피와 차를 제공하는 커피전문점입니다. 
+커피와 차를 기본으로 함께 할 수 있는 Cake과 Bagel, Sandwich 등의 다양한 베이커리를 판매하고 있으며,
+매장 밖에서도 커피빈을 느낄 수 있게 해주는 각종 커피관련 제품들을 판매하며 
+''커피와 차를 즐길 수 있는 모든 길''을 제시하고 있습니다.',19);
+Insert into FRANCHISE (FRANCHISE_NUM,FRANCHISE_NAME,FRANCHISE_IMG,FRANCHISE_INTRODUCE,FRANCHISE_VISIT) values (7,'pascucci','pascucci.png','1883년 이탈리아 몬테펠트로에서 탄생한 카페 파스쿠찌. 현재 이태리, 영국, 스페인,
+미국 등 세계 20여 개 국의 40개 도시에 진출한 글로벌 브랜드로 성장하였습니다.
+한국에서는 지난 2001년 식품 전문 기업인 SPC그룹의 계열사인
+파리크라상이 파스쿠찌 이태리 본사와 브랜드 도입계약을 맺고
+현재 전국적으로 브랜드를 운영하여 발전해 왔습니다.',44);
+Insert into FRANCHISE (FRANCHISE_NUM,FRANCHISE_NAME,FRANCHISE_IMG,FRANCHISE_INTRODUCE,FRANCHISE_VISIT) values (8,'yogerpresso','yogerpresso.png','''요거프레소''는 ''yogurt''와 ''espresso''의 합성어로, 진한 커피와 특제 요거트 아이스크림을 즐길 수 있는  프리미엄 디저트 카페임을 함축하고 있는 브랜드입니다.
+신메뉴 출시마다 SNS상에서 화제를 모으며 브랜드 경쟁력을 높이고 있는 요거프레소. 그 뒤에는 TV/라디오 광고, 드라마 PPL, 문화마케팅, 스타마케팅, 신메뉴 프로모션, SNS마케팅 등 브랜드 컨셉과 인지도를 극대화시키는 마케팅 전략이 있습니다. 고객 방문을 이끄는 힘, 바로 ''요거프레소''라는 브랜드 가치에 있습니다.',7);
